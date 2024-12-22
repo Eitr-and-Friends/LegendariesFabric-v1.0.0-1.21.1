dbf5bc62740a3a124628a6411e0411164f989f9f
Eitr <danilo.mayuga.iii@gmail.com>
2024-12-22 12:38:13 +0800

    idk

---
+++ src/main/java/legendaries/mod/LegendariesClient.java
@@ -0,0 +1,10 @@
+package legendaries.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LegendariesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
